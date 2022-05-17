f153d4516fa886681b0589809246c646ad9f6ed1
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2022-05-17 02:29:10 +0000

    Scrapped the djs command due to the lack of use cases and replaced it with a channel-info command.

--- interactions/commands/Info/djs.js
+++ interactions/commands/Info/djs.js
@@ -1,29 +1,69 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { getRoleColor } = require('../../../utils/getRoleColor');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('djs')
-    .setDescription(`Searches the Discord.js Documentation`)
-    .addUserOption((option) => option
-      .setName('term')
-      .setDescription('Term to search in the documentation')
+    .setName('channelinfo')
+    .setDescription(`Gets Info about a channel in the server`)
+    .addChannelOption((option) => option
+      .setName('channel')
+      .setDescription('channel to get the info of')
       .setRequired(true),
     ),
   cooldown: 5000,
  async execute(interaction) {
-   const query = interaction.options.getString('query');
-		try {
-			const req = await axios.get(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`);
-			const data = req.data;
-			if (data && !data.error) {
-				interaction.reply({ embeds: [data] });
-			} else {
-				return interaction.rpely({ content: `:x: There was an error`, ephemeral: true });
-			}
-		} catch (e) {
-			return interaction.reply({ content: e, ephemeral: true });
+   const channel = interaction.options.getChannel('channel');
+   const color = getRoleColor(interaction.guild)
+		const embed = new MessageEmbed().setTitle(`${channel.name} Info`);
+		if (channel.isText && channel.topic) {
+			embed.setDescription(channel.topic);
 		}
+		if (channel.rateLimitPerUser) {
+			embed.addField('Slow Mode:', `${channel.rateLimitPerUser} Secounds`, true);
+		}
+		if (channel.parent) {
+			embed.addField('Catgory Name:', channel.parent.name);
+		}
+		if (channel.lastPinTimestamp) {
+			embed.addField('Last Pin Message At:', `<t:${Math.floor(channel.lastPinTimestamp / 1000)}:R>`, true);
+		}
+		let channelTypes;
+		switch (channel.type) {
+			case 'GUILD_TEXT':
+				channelTypes = 'Text Channel';
+				break;
+			case 'GUILD_VOICE':
+				channelTypes = 'Voice Channel';
+				break;
+			case 'GUILD_CATEGORY':
+				channelTypes = 'Category Channel';
+				break;
+			case 'GUILD_NEWS':
+				channelTypes = 'News Channel';
+				break;
+			case 'GUILD_STORE':
+				channelTypes = 'Store Channel';
+				break;
+			case 'GUILD_NEWS_THREAD':
+				channelTypes = 'News Thread Channel';
+				break;
+			case 'GUILD_PUBLIC_THREAD':
+				channelTypes = 'Public Thread Channel';
+				break;
+			case 'GUILD_PRIVATE_THREAD':
+				channelTypes = 'Private Thread Channel';
+				break;
+			case 'GUILD_STAGE_VOICE':
+				channelTypes = 'Stage Channel';
+				break;
+		}
+		embed.addField('Channel Type:', channelTypes, true);
+		embed.addField('Channel Created At:', `<t:${Math.floor(channel.createdTimestamp / 1000)}:R>`, true);
+		embed.setColor(color);
+		embed.setFooter(channel.id);
+		interaction.reply({
+			embeds: [embed],
+		});
   }
 }
