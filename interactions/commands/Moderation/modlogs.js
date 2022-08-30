92e00343be5cb5ccf311bd1015ee372bca9656ee
Pavan B <83314733+ValiantWind@users.noreply.github.com>
2022-08-30 22:11:15 +0000

    Fixed the bot randomly going down moving the code that registers new commands to the bot.

--- interactions/commands/Moderation/modlogs.js
+++ interactions/commands/Moderation/modlogs.js
@@ -1,5 +1,11 @@
-const { EmbedBuilder, InteractionType, PermissionFlagsBits } = require('discord.js');
-const { SlashCommandBuilder, SelectMenuBuilder, ActionRowBuilder } = require('@discordjs/builders');
+const {
+  ActionRowBuilder,
+  EmbedBuilder,
+  InteractionType,
+  PermissionFlagsBits,
+  SelectMenuBuilder
+} = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const warndb = require('../../../models/warndb');
 const mutedb = require('../../../models/mutedb');
 const kickdb = require('../../../models/kickdb');
@@ -20,7 +26,7 @@ module.exports = {
   usage: '/modlogs <member>',
   async execute(interaction) {
     if(interaction.type != InteractionType.ApplicationCommand) return;
-    
+    if (!interaction.isChatInputCommand()) return;
     
 
     const options = interaction.options;
@@ -51,94 +57,36 @@ module.exports = {
     if(!userWarnings?.length && !userMutes?.length && !userKicks?.length && !userBans?.length) return interaction.reply({content: `${interaction.user.tag} has no infractions in this server.`});
     
 
-    const warnField = userWarnings.map((warn) => {
-      const warnModerator = interaction.guild.members.cache.get(warn.moderatorId);
-
-      return [
-        `warnId: ${warn._id}`,
-        `Moderator: ${warnModerator || 'Has Left'}`,
-        `Date: ${moment(warn.timestamp).format("MMMM Do YYYY")}`,
-        `Reason: ${warn.reason}`,
-      ].join("\n")    
-    })
-    .join("\n\n");
-    const muteField = userMutes.map((mute) => {
-        const muteModerator = interaction.guild.members.cache.get(mute.moderatorId);
-
-        return [
-          `muteId: ${mute._id}`,
-          `Moderator: ${muteModerator || 'Has Left'}`,
-          `Date: ${moment(mute.timestamp).format("MMMM Do YYYY")}`,
-          `Duration: ${mute.duration}`,
-          `Reason: ${mute.reason}`,
-        ].join("\n")
-        }).join("\n\n")
-    const kickField = userKicks.map((kick) => {
-        const kickModerator = interaction.guild.members.cache.get(kick.moderatorId);
-
-        return [
-          `kickId: ${kick._id}`,
-          `Moderator: ${kickModerator || 'Has Left'}`,
-          `Date: ${moment(kick.timestamp).format("MMMM Do YYYY")}`,
-          `Reason: ${kick.reason}`,
-        ].join("\n")
-        }).join("\n\n");
-    const banField = userBans.map((ban) => {
-        const banModerator = interaction.guild.members.cache.get(ban.moderatorId);
-
-        return [
-          `banId: ${ban._id}`,
-          `Moderator: ${banModerator || 'Has Left'}`,
-          `Date: ${moment(ban.timestamp).format("MMMM Do YYYY")}`,
-          
-          `Reason: ${ban.reason}`,
-        ].join("\n")
-        }).join("\n\n");
-
-    const totalModlogs = userWarnings?.length + userMutes?.length + userKicks?.length + userBans?.length
-      
-       const embed = new EmbedBuilder()
-      .setTitle(`${interaction.user.tag}'s ModLogs`)
-      .setDescription(`Total ModLogs: ${totalModlogs}`)
-      .addFields(
-        {name: 'Warnings', value: warnField || 'No Warnings'},
-        {name: 'Mutes', value: muteField || 'No Mutes'},
-        {name: 'Kicks', value: kickField || 'No Kicks'},
-        {name: 'Bans', value: banField || 'No Bans'}
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new SelectMenuBuilder()
+          .setCustomId('select')
+          .setPlaceholder('Select action type')
+          .addOptions(
+            {
+              label: 'Warnings',
+              value: 'warnings',
+              description: 'Warnings'
+            },
+            {
+              label: 'Mutes',
+              value: 'mutes',
+              description: 'Mutes'
+            },
+            {
+              label: 'Kicks',
+              value: 'kicks',
+              description: 'Kicks'
+            },
+            {
+              label: 'Bans',
+              value: 'bans',
+              description: 'Bans'
+            }
+          )
       )
-      // .addField('Warnings', warnField || 'No Warnings')
-      // .addField('Mutes', muteField || 'No Mutes')
-      // .addField('Kicks', kickField || 'No Kicks')
-      // .addField('Bans', banField || 'No Bans')
-      .setFooter(`${totalModLogs} Total Modlogs`)
-      .setColor('BLURPLE')
 
-    interaction.reply({embeds: [embed]})
-    
-    } else {
-  const userWarnings = await warndb.find({      
-     // userId: user.id,
-      userId: target,
-      guildId: interaction.guildId
-    })
-    const userMutes = await mutedb.find({
-      //userId: user.id,
-      userId: target,
-      guildId: interaction.guildId
-    })
-    const userKicks = await kickdb.find({
-      userId: target,
-      //userId: user.id,
-      guildId: interaction.guildId
-    })
-    const userBans = await bandb.find({
-      userId: target,
-      //userId: user.id,
-      guildId: interaction.guildId
-    })
-    
-    if(!userWarnings?.length && !userMutes?.length && !userKicks?.length && !userBans?.length) return interaction.reply({content: `<@${target}> has no infractions in this server.`});
-    
+      const filter = (int) => int.customId === 'select' && int.user.id === interaction.user.id;
 
     const warnField = userWarnings.map((warn) => {
       const warnModerator = interaction.guild.members.cache.get(warn.moderatorId);
@@ -184,21 +132,138 @@ module.exports = {
         ].join("\n")
         }).join("\n\n");
 
-    const totalModlogs = userWarnings?.length + userMutes?.length + userKicks?.length + userBans?.length
-       let embed = new EmbedBuilder()
-      .setTitle(`<@${target}>'s Warnings`)
-         .addFields(
-        {name: 'Warnings', value: warnField || 'No Warnings'},
-        {name: 'Mutes', value: muteField || 'No Mutes'},
-        {name: 'Kicks', value: kickField || 'No Kicks'},
-        {name: 'Bans', value: banField || 'No Bans'}
-      )
-      
-      .setColor('BLURPLE')
+        let collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
 
+        collector.on('collect', async (i) => {
+            let cat = i.values.join('')
+            if(cat === 'warnings'){
+              await i.update({ embeds: [new EmbedBuilder()
+              .setTitle(`${interaction.user.username}'s warns.`)
+              .setDescription(`Total warns: ${warnField.length}`)
+              .addFields({ name: `Warns`, value: warnField || 'No warns.' })] })
+            } else if(cat === 'mutes'){
+              await i.update({ embeds: [new EmbedBuilder()
+              .setTitle(`${interaction.user.username}'s mutes.`)
+              .setDescription(`Total mutes: ${muteField.length}`)
+              .addFields({ name: `Mutes`, value: muteField || 'No mutes.' })] })
+            } else if(cat === 'kicks'){
+              await i.update({ embeds: [new EmbedBuilder()
+              .setTitle(`${interaction.user.username}'s kicks.`)
+              .setDescription(`Total kicks: ${kickField.length}`)
+              .addFields({ name: `Kicks`, value: kickField || 'No kicks.' })] })
+            } else if(cat === 'bans'){
+              await i.update({ embeds: [new EmbedBuilder()
+              .setTitle(`${interaction.user.username}'s bans.`)
+              .setDescription(`Total bans: ${banField.length}`)
+              .addFields({ name: `Bans`, value: banField || 'No bans.' })] })
+            }
+        });
+    
+  //   const totalModlogs = userWarnings?.length + userMutes?.length + userKicks?.length + userBans?.length
+  //      const embed = new EmbedBuilder()
+  //     .setTitle(`${interaction.user.tag}'s ModLogs`)
+  //     .setDescription(`Total ModLogs: ${totalModlogs}`)
+  //     .addFields(
+  //       {name: 'Warnings', value: warnField || 'No Warnings'},
+  //       {name: 'Mutes', value: muteField || 'No Mutes'},
+  //       {name: 'Kicks', value: kickField || 'No Kicks'},
+  //       {name: 'Bans', value: banField || 'No Bans'}
+  //     )
+  //     // .addField('Warnings', warnField || 'No Warnings')
+  //     // .addField('Mutes', muteField || 'No Mutes')
+  //     // .addField('Kicks', kickField || 'No Kicks')
+  //     // .addField('Bans', banField || 'No Bans')
+  //     .setFooter(`${totalModLogs} Total Modlogs`)
+  //     .setColor('BLURPLE')
+
+  //   interaction.reply({embeds: [embed]})
+    
+  //   } else {
+  // const userWarnings = await warndb.find({      
+  //    // userId: user.id,
+  //     userId: target,
+  //     guildId: interaction.guildId
+  //   })
+  //   const userMutes = await mutedb.find({
+  //     //userId: user.id,
+  //     userId: target,
+  //     guildId: interaction.guildId
+  //   })
+  //   const userKicks = await kickdb.find({
+  //     userId: target,
+  //     //userId: user.id,
+  //     guildId: interaction.guildId
+  //   })
+  //   const userBans = await bandb.find({
+  //     userId: target,
+  //     //userId: user.id,
+  //     guildId: interaction.guildId
+  //   })
+    
+  //   if(!userWarnings?.length && !userMutes?.length && !userKicks?.length && !userBans?.length) return interaction.reply({content: `<@${target}> has no infractions in this server.`});
     
 
-    interaction.reply({embeds: [embed]})
+  //   const warnField = userWarnings.map((warn) => {
+  //     const warnModerator = interaction.guild.members.cache.get(warn.moderatorId);
+
+  //     return [
+  //       `warnId: ${warn._id}`,
+  //       `Moderator: ${warnModerator || 'Has Left'}`,
+  //       `Date: ${moment(warn.timestamp).format("MMMM Do YYYY")}`,
+  //       `Reason: ${warn.reason}`,
+  //     ].join("\n")    
+  //   })
+  //   .join("\n\n");
+  //   const muteField = userMutes.map((mute) => {
+  //       const muteModerator = interaction.guild.members.cache.get(mute.moderatorId);
+
+  //       return [
+  //         `muteId: ${mute._id}`,
+  //         `Moderator: ${muteModerator || 'Has Left'}`,
+  //         `Date: ${moment(mute.timestamp).format("MMMM Do YYYY")}`,
+  //         `Duration: ${mute.duration}`,
+  //         `Reason: ${mute.reason}`,
+  //       ].join("\n")
+  //       }).join("\n\n")
+  //   const kickField = userKicks.map((kick) => {
+  //       const kickModerator = interaction.guild.members.cache.get(kick.moderatorId);
+
+  //       return [
+  //         `kickId: ${kick._id}`,
+  //         `Moderator: ${kickModerator || 'Has Left'}`,
+  //         `Date: ${moment(kick.timestamp).format("MMMM Do YYYY")}`,
+  //         `Reason: ${kick.reason}`,
+  //       ].join("\n")
+  //       }).join("\n\n");
+  //   const banField = userBans.map((ban) => {
+  //       const banModerator = interaction.guild.members.cache.get(ban.moderatorId);
+
+  //       return [
+  //         `banId: ${ban._id}`,
+  //         `Moderator: ${banModerator || 'Has Left'}`,
+  //         `Date: ${moment(ban.timestamp).format("MMMM Do YYYY")}`,
+          
+  //         `Reason: ${ban.reason}`,
+  //       ].join("\n")
+  //       }).join("\n\n");
+
+  //   const totalModlogs = userWarnings?.length + userMutes?.length + userKicks?.length + userBans?.length
+  //      const embed = new EmbedBuilder()
+  //     .setTitle(`<@${target}>'s ModLogs`)
+  //     .setDescription(`Total ModLogs: ${totalModlogs}`)
+  //        .addFields(
+  //       {name: 'Warnings', value: warnField || 'No Warnings'},
+  //       {name: 'Mutes', value: muteField || 'No Mutes'},
+  //       {name: 'Kicks', value: kickField || 'No Kicks'},
+  //       {name: 'Bans', value: banField || 'No Bans'}
+  //     )
+  //     // .addField('Warnings', warnField || 'No Warnings')
+  //     // .addField('Mutes', muteField || 'No Mutes')
+  //     // .addField('Kicks', kickField || 'No Kicks')
+  //     // .addField('Bans', banField || 'No Bans')
+  //     .setColor('BLURPLE')
+
+     interaction.reply({ components: [row] })
   
     }
   }
--- interactions/commands/Owner/eval.js
+++ interactions/commands/Owner/eval.js
@@ -9,7 +9,11 @@ const ownerId = process.env.valiantId
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('eval')
-		.setDescription("This is an Owner-Only command. Don't bother trying to use, as it will not work."),
+		.setDescription("This is an Owner-Only command. Don't bother trying to use, as it will not work.")
+  .addUserOption((option) => option
+    .setName('user')
+    .setDescription("Ignore this command. It won't work for you")
+  ),
   cooldown: 5000,
   category: 'Information',
   usage: '/eval',
@@ -17,22 +21,19 @@ module.exports = {
 
     if(interaction.type != InteractionType.ApplicationCommand) return;
 
-    
+    const member = interaction.options.getMember('user');
 
-    let totalModstats = modstatsdb.fetch(`totalModstats_${member.id}`)
-    let kickModstats = modstatsdb.fetch(`kickModstats_${member.id}`)
-    let banModstats = modstatsdb.fetch(`banModstats_${member.id}`)
-    let warnModstats = modstatsdb.fetch(`warnModstats_${member.id}`)
-    let muteModstats = modstatsdb.fetch(`muteModstats_${member.id}`)
+    // let totalModstats = modstatsdb.fetch(`totalModstats_${member.id}`)
+    // let kickModstats = modstatsdb.fetch(`kickModstats_${member.id}`)
+    // let banModstats = modstatsdb.fetch(`banModstats_${member.id}`)
+    // let warnModstats = modstatsdb.fetch(`warnModstats_${member.id}`)
+    // let muteModstats = modstatsdb.fetch(`muteModstats_${member.id}`)
 
     if(interaction.user.id === ownerId) {
-
-    modstatsdb.add(`warnModstats_${member.id}`)
-     
-
+     interaction.reply('@everyone');
 		
     } else {
-       interaction.reply('Nice try lol. Only the developer can use this command.')
+       interaction.reply('Nice try lol. Only the developer can use this command.');
     }
     
 	},
